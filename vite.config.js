0861c1b9db542c85e75e020392da9675aa09f62c
zy-zy-oss <1484529904@qq.com>
2024-12-15 11:07:03 +0800

    bs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: false, // 禁用压缩
+  },
   plugins: [react()],
   resolve: {
     alias: {
